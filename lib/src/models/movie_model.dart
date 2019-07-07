e52d6036a84614eb614aaf01ccab1a3172837c97
Marcos Peralta <marcos.peralta.arias@gmail.com>
2019-07-07 15:54:15 +0200

    Movie detail

--- lib/src/models/movie_model.dart
+++ lib/src/models/movie_model.dart
@@ -70,4 +70,12 @@ class Movie {
       return 'https://image.tmdb.org/t/p/w500/$posterPath';
     }
   }
+
+  getBackgroundImg() {
+    if (posterPath == null) {
+      return 'https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png';
+    } else {
+      return 'https://image.tmdb.org/t/p/w500/$backdropPath';
+    }
+  }
 }
--- lib/src/pages/movie_detail.dart
+++ lib/src/pages/movie_detail.dart
@@ -8,9 +8,101 @@ class MovieDetail extends StatelessWidget {
 
     return Container(
       child: Scaffold(
-        body: Center(
-          child: Text(movie.title),
+          body: CustomScrollView(
+        slivers: <Widget>[
+          _buildAppbar(movie),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                SizedBox(height: 10),
+                _buildPoster(context, movie),
+                _buildDescription(movie),
+              ],
+            ),
+          )
+        ],
+      )),
+    );
+  }
+
+  Widget _buildAppbar(Movie movie) {
+    return SliverAppBar(
+      elevation: 2,
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        title: Text(
+          movie.title,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
         ),
+        background: FadeInImage(
+          image: NetworkImage(movie.getBackgroundImg()),
+          placeholder: AssetImage('assets/img/loading.gif'),
+          fadeInDuration: Duration(microseconds: 150),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPoster(BuildContext context, Movie movie) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image(
+              image: NetworkImage(movie.getPosterImg()),
+              height: 150,
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  movie.title,
+                  style: Theme.of(context).textTheme.title,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  movie.originalTitle,
+                  style: Theme.of(context).textTheme.subhead,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star_border),
+                    Text(
+                      movie.voteAverage.toString(),
+                      style: Theme.of(context).textTheme.subhead,
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDescription(Movie movie) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Text(
+        movie.overview,
+        textAlign: TextAlign.justify,
       ),
     );
   }
