ef441e85227171b8948d5ef604f9628b3dbba678
Marcos Peralta <marcos.peralta.arias@gmail.com>
2019-07-07 14:26:48 +0200

    Refactor app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/pages/home_page.dart';
 
-void main() => runApp(PeliculasApp());
+void main() => runApp(MoviesApp());
 
-class PeliculasApp extends StatelessWidget {
-  const PeliculasApp({Key key}) : super(key: key);
+class MoviesApp extends StatelessWidget {
+  const MoviesApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
